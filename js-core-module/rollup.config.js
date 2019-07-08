b919286fc568a8b1addc4cda23f31d7f7402a299
Ryutsasi <ryutsashi@gmail.com>
2019-07-08 04:57:46 +0200

    rollup output files fix, rollup moved to devDependencies

--- js-core-module/rollup.config.js
+++ js-core-module/rollup.config.js
@@ -1,12 +1,14 @@
 export default {
     input: 'src/js-core.js',
-    output: {
-        file: 'target/build/js-core.module.js',
-        format: 'es'
-    },
-    output: {
-        file: 'target/build/js-core.js',
-        format: 'umd',
-        name: 'jsCore'
-    }
+    output: [
+        {
+            file: 'target/build/js-core.module.js',
+            format: 'es'
+        },
+        {
+            file: 'target/build/js-core.js',
+            format: 'umd',
+            name: 'jsCore'
+        }
+    ]
 }
